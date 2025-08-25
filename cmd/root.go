471355ed14ff47fa1b8d1c230bc64bea5da9eade
Matt Krueger <mkrueger@rstms.net>
2025-08-25 00:48:07 -0600

    Bump version: 0.0.2 → 0.0.3

--- cmd/root.go
+++ cmd/root.go
@@ -29,7 +29,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.2",
+	Version: "0.0.3",
 	Use:     "notify",
 	Short:   "display a notification on the user desktop",
 	Long: `
--- notify/version.go
+++ notify/version.go
@@ -1,5 +1,5 @@
 package notify
 
-const Version = "0.0.2"
+const Version = "0.0.3"
 
 //foo
